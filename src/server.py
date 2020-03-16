05129211345d35beae6ed6991b38d9994a12155a
Aryan Badiee <51372926+aryanbadiee@users.noreply.github.com>
2020-03-16 05:52:37 +0330

    multi-thread is supported

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@
 import socket  # Networking support
 import time  # Current time
 import _thread  # multi-threading
+import threading  # new library of multi-thread
 
 
 myPort = 80  # your own port
@@ -27,16 +28,16 @@ def get_content_type(file_requested: str) -> str:
 
 
 # for getting data from body of HTTP:
-def read_body_data(string: str) -> str:
-    lines = string.splitlines()
+def read_body_data(http: str) -> str:
+    lines = http.splitlines()
 
     result = ""  # for return
-    _next = False
+    flag = False
 
     for line in lines:
         if line == "":  # means empty line between header and body in HTTP
-            _next = True
-        elif _next:
+            flag = True
+        elif flag:
             result += line
 
     return result
@@ -76,10 +77,11 @@ def gen_headers(code, _type: str = None) -> str:
 class Server:
     """ Class describing a simple HTTP server objects."""
 
-    def __init__(self):
+    def __init__(self, host: str = myHost, port: int = myPort):
         """ Constructor """
-        self.host = myHost  # <-- works on all available network interfaces
-        self.port = myPort
+
+        self.host = host
+        self.port = port
         self.root = myRoot  # Directory where web-page files are stored
 
         """ Attempts to acquire the socket and launch the server """
@@ -89,7 +91,7 @@ class Server:
         # AF_INET = using ipv4
         # AF_INET6 = using ipv6
 
-    def activate_server(self):
+    def run(self):
         # user provided in the __init__() port may be unavailable:
         try:
             self.socket.bind((self.host, self.port))
@@ -113,123 +115,129 @@ class Server:
                 sys.exit(1)
 
         print("Server acquired successfully the socket with port:", self.port)
-        print("----------------------------------------")
-        self.wait_for_connections()
+        print("-----------------------------------------------------")
+
+        # listen:
+        self.socket.listen(5)  # maximum number of queued connections
+
+        # accept:
+        while True:
+            print("Awaiting new connection", "-->", end=" ")
+
+            connection, address = self.socket.accept()
+            # connection - socket (socket)
+            # address - client address (tuple)
+            # address[0] = ip (str)
+            # address[1] = port (int)
+
+            print("Got connection from:", address[0] + ":" + str(address[1]))
+            threading.Thread(target=self.handler, args=(connection, )).start()  # run thread
+
+    def handler(self, connection):
+        data = connection.recv(1024)  # receive data from client
+        string = bytes.decode(data)  # decode it to string
+
+        request_method = string.split()[0]  # method of request
+        # print("Method: ", request_method)
+        # print("*** HTTP DATA RECEIVED ***", string, sep='\n', end="\n\n")
+
+        if (request_method == 'GET') or (request_method == 'HEAD') or (request_method == 'POST'):
+
+            file_requested = string.split()[1]  # getting 2nd element(request of client)
+            response_content = b""  # for body of http response(it's binary)
+
+            if file_requested == '/' or \
+                    file_requested == '/index' or \
+                    file_requested == '/main':  # in case no file is specified by the browser
+
+                file_requested = 'index.html'  # load index.html by default
+
+                # file_requested = self.www_dir + file_requested
+                # print("Serving web page [", file_requested, "]")
+
+                # Load file content
+                try:
+                    file_requested = self.root + file_requested  # public/...
+                    with open(file_requested, "rb") as file_handler:
+                        if request_method == "GET" or request_method == "POST":
+                            response_content = file_handler.read()
+
+                    response_headers = gen_headers(200,
+                                                   get_content_type(file_requested))
+                except Exception as e:  # in case file was not found, generate 404 page
+                    # print("Warning, file not found. Serving response code 404\n", e, sep='')
+                    response_headers = gen_headers(404)
+                    if request_method == 'GET' or request_method == 'POST':
+                        response_content = \
+                            b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
+
+                server_response = response_headers.encode()  # return headers for GET, POST and HEAD
+                if request_method == 'GET' or request_method == 'POST':
+                    server_response += response_content  # return additional content for GET, POST only
+
+                connection.send(server_response)
+                # print("Closing connection with client")
+                connection.close()
+
+            elif file_requested == "/msg":
+                data_of_body = read_body_data(string)  # body_data: str
+                if (index_of_variable := data_of_body.find("text=")) != -1:
+                    index_of_value = index_of_variable + len("text=")
+                    client_msg = data_of_body[index_of_value:]
+
+                    header_resp = gen_headers(200)
+                    body_resp = client_msg
+
+                    resp = header_resp + body_resp
+                    resp = resp.encode("UTF-8")
+
+                    connection.send(resp)
+                connection.close()
+
+            else:
+                file_requested = file_requested[1:]  # Removing '/'
+
+                # print("Serving web page [", file_requested, "]")
+
+                # Load file content:
+                try:
+                    file_requested = self.root + file_requested  # public/...
+                    with open(file_requested, "rb") as file_handler:
+                        if request_method == "GET" or request_method == "POST":
+                            response_content = file_handler.read()
+
+                    response_headers = gen_headers(200,
+                                                   get_content_type(file_requested))
+                except Exception as e:  # in case file was not found, generate 404 page
+                    # print("Warning, file not found. Serving response code 404\n", e, sep='')
+                    response_headers = gen_headers(404)
+                    if request_method == 'GET' or request_method == 'POST':
+                        response_content = \
+                            b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
+
+                server_response = response_headers.encode()  # return headers for GET, Post and HEAD
+                if request_method == 'GET' or request_method == 'POST':
+                    server_response += response_content  # return additional content for GET, POST only
+
+                connection.send(server_response)
+                # print("Closing connection with client")
+                connection.close()
+
+            # print("----------------------------------------")
 
     def shutdown(self):
         """ Shut down the server """
+
         try:
             print("Shutting down the server")
             self.socket.shutdown(socket.SHUT_RDWR)
         except Exception as e:
             print("Warning: could not shut down the socket. Maybe it was already closed?", e)
 
-    def wait_for_connections(self):
-        """ Main loop awaiting connections """
-        self.socket.listen(5)  # maximum number of queued connections
-        while True:
-            print("Awaiting New Connection")
-
-            connection, address = self.socket.accept()
-            # connection - socket
-            # address - client address
-
-            print("Got connection from:", address)
-
-            data = connection.recv(1024)  # receive data from client
-            string = bytes.decode(data)  # decode it to string
-
-            request_method = string.split()[0]  # method of request
-            print("Method: ", request_method)
-            print("*** HTTP DATA RECEIVED ***", string, sep='\n', end="\n\n")
-
-            if (request_method == 'GET') or (request_method == 'HEAD') or (request_method == 'POST'):
-
-                file_requested = string.split()[1]  # getting 2nd element(request of client)
-                response_content = b""  # for body of http response(it's binary)
-
-                if file_requested == '/' or \
-                        file_requested == '/index' or \
-                        file_requested == '/main':  # in case no file is specified by the browser
-
-                    file_requested = 'index.html'  # load index.html by default
-
-                    # file_requested = self.www_dir + file_requested
-                    print("Serving web page [", file_requested, "]")
-
-                    # Load file content
-                    try:
-                        file_requested = self.root + file_requested  # public/...
-                        with open(file_requested, "rb") as file_handler:
-                            if request_method == "GET" or request_method == "POST":
-                                response_content = file_handler.read()
-
-                        response_headers = gen_headers(200,
-                                                       get_content_type(file_requested))
-                    except Exception as e:  # in case file was not found, generate 404 page
-                        print("Warning, file not found. Serving response code 404\n", e, sep='')
-                        response_headers = gen_headers(404)
-                        if request_method == 'GET' or request_method == 'POST':
-                            response_content = \
-                                b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
-
-                    server_response = response_headers.encode()  # return headers for GET, POST and HEAD
-                    if request_method == 'GET' or request_method == 'POST':
-                        server_response += response_content  # return additional content for GET, POST only
-
-                    connection.send(server_response)
-                    print("Closing connection with client")
-                    connection.close()
-
-                elif file_requested == "/msg":
-                    data_of_body = read_body_data(string)  # body_data: str
-                    if (index_of_variable := data_of_body.find("text=")) != -1:
-                        index_of_value = index_of_variable + len("text=")
-                        client_msg = data_of_body[index_of_value:]
-
-                        header_resp = gen_headers(200)
-                        body_resp = client_msg
-
-                        resp = header_resp + body_resp
-                        resp = resp.encode("UTF-8")
-
-                        connection.send(resp)
-                    connection.close()
-
-                else:
-                    file_requested = file_requested[1:]  # Removing '/'
-
-                    print("Serving web page [", file_requested, "]")
-
-                    # Load file content:
-                    try:
-                        file_requested = self.root + file_requested  # public/...
-                        with open(file_requested, "rb") as file_handler:
-                            if request_method == "GET" or request_method == "POST":
-                                response_content = file_handler.read()
-
-                        response_headers = gen_headers(200,
-                                                       get_content_type(file_requested))
-                    except Exception as e:  # in case file was not found, generate 404 page
-                        print("Warning, file not found. Serving response code 404\n", e, sep='')
-                        response_headers = gen_headers(404)
-                        if request_method == 'GET' or request_method == 'POST':
-                            response_content = \
-                                b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
-
-                    server_response = response_headers.encode()  # return headers for GET, Post and HEAD
-                    if request_method == 'GET' or request_method == 'POST':
-                        server_response += response_content  # return additional content for GET, POST only
-
-                    connection.send(server_response)
-                    print("Closing connection with client")
-                    connection.close()
-
-                print("----------------------------------------")
-
     def graceful_shutdown(self):
         """ This function shuts down the server. It's triggered
         by SIGINT signal """
+
         self.shutdown()  # shut down the server
         import sys
         sys.exit(1)
@@ -252,5 +260,5 @@ _thread.start_new_thread(exit_process, ())  # for calling exit_process in other 
 
 print("Starting Web Server")
 print("if you want to exit from program just write \"$exit\" in console!")
-_server = Server()  # construct server object
-_server.activate_server()  # acquire the socket
+serv = Server("0.0.0.0", 80)  # construct server object
+serv.run()  # acquire the socket
